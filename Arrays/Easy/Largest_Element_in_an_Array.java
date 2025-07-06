ce6baf2b360a5c2e47b68eeef2d466fc4b321506
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-07-06 20:01:10 +0530

    Update Largest_Element_in_an_Array.java

--- Arrays/Easy/Largest_Element_in_an_Array.java
+++ Arrays/Easy/Largest_Element_in_an_Array.java
@@ -1,13 +1,29 @@
+/*
+Given an array of integers nums, return the value of the largest element in the array
+
+Examples:
+Input: nums = [3, 3, 6, 1]
+
+Output: 6
+
+Explanation: The largest element in array is 6
+
+Input: nums = [3, 3, 0, 99, -40]
+
+Output: 99
+
+Explanation: The largest element in array is 99
+*/
+
 class Solution {
     public int findLargest(int[] nums) {
-        int largest = nums[0]; // assume first element is the largest
+        int largest = nums[0]; 
 
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] > largest) {
-                largest = nums[i]; // update if current element is greater
+                largest = nums[i]; 
             }
         }
-
         return largest;
     }
 }
