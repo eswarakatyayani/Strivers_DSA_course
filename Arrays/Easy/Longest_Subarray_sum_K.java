b9532f2ce350b370ef1c1607e4c040d57e474487
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-08-17 21:24:24 +0530

    Create Longest_Subarray_sum_K.java

---
+++ Arrays/Easy/Longest_Subarray_sum_K.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int subarraySum(int[] nums, int target) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       int sum=0;
+       int max=0;
+       for(int i=0;i<nums.length;i++)
+       {
+        sum+=nums[i];
+            if (sum == target) {
+                max = i + 1;
+            }
+        if(map.containsKey(sum-target))
+        {
+        max= Math.max(max,i+1-map.get(sum-target));
+        }
+        if (!map.containsKey(sum)) 
+        map.put(sum,i);
+       }
+       return max;
+
+    }
+}
