414fa09eac2278eb62513dea5290916a04d3c10c
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-09-20 13:55:41 +0530

    Update Largest_Odd_Number_Substring_in_String.java

--- Strings/Easy/Largest_Odd_Number_Substring_in_String.java
+++ Strings/Easy/Largest_Odd_Number_Substring_in_String.java
@@ -1,4 +1,13 @@
 /*
+Largest Odd Number in a String
+Given a string s, representing a large integer, the task is to return the largest-valued odd integer (as a string) that is a substring of the given string s.
+The number returned should not have leading zero's. But the given input string may have leading zero. (If no odd number is found, then return empty string.)
+Examples:
+Input : s = "5347"
+Output : "5347"
+*/
+
+/*
 Look at the last digit of the string.
 If it is odd, the entire string is already the largest odd number. return it.
 If it is even, ignore the last and check the previous char.
