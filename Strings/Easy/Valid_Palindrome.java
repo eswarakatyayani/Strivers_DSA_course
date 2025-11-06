79c0fb6d2a4515f1438308c57ccb66b477cd716e
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-11-06 13:44:27 +0530

    palindrome checking
    Implement a method to check if a string is a palindrome by ignoring case and non-alphanumeric characters.

---
+++ Strings/Easy/Valid_Palindrome.java
@@ -0,0 +1,38 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+*/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder pal = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch>='0' && ch<='9')) {
+                pal.append(Character.toLowerCase(ch));
+            }
+        }
+        String original = pal.toString();
+        String reversed = new StringBuilder(original).reverse().toString();
+
+        return original.equals(reversed);
+    }
+}
