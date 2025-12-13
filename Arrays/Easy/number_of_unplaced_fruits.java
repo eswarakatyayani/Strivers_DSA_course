7d27acc96c96c24152276093a72e294252444d69
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-12-13 14:34:27 +0530

    Add number_of_unplaced_fruits.java solution file

---
+++ Arrays/Easy/number_of_unplaced_fruits.java
@@ -0,0 +1,23 @@
+//3477. Fruits Into Baskets II
+
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int ans = 0;
+        int n = baskets.length;
+
+        for(int f : fruits){
+            int j = 0;
+            while(j<n){
+                if(baskets[j] >= f){
+                    baskets[j] = 0;
+                    break;
+                }
+                j++;
+            }
+            if(j == n)
+                ans++;
+        }
+
+        return ans;
+    }
+}
