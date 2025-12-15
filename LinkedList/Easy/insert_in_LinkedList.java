dc018a466e50440b2ed57c36163e9d01794fc516
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-12-15 11:54:59 +0530

    Add insertAtHead method to insert node at head
    Implemented a method to insert a new node at the head of a singly linked list.

--- LinkedList/Easy/insert_in_LinkedList.java
+++ LinkedList/Easy/insert_in_LinkedList.java
@@ -1,2 +1,20 @@
 /*
+Insert new element at begining:
+Definition of singly linked list:
+class ListNode{
+    public int data;
+    public ListNode next;
+    ListNode() { data = 0; next = null; }
+    ListNode(int x) { data = x; next = null; }
+    ListNode(int x, ListNode next) { data = x; this.next = next; }
+}
 */
+
+class Solution {
+    public ListNode insertAtHead(ListNode head, int X) {
+        ListNode newNode = new ListNode(X);
+        newNode.next = head;
+        return newNode;
+    }
+}
+
