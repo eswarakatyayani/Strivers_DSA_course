908cc17d563640644fab51178bd830958aa083aa
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-12-20 12:47:51 +0530

    Add cycle detection method for linked list
    Implement cycle detection in a singly linked list using a hashmap.

---
+++ LinkedList/Medium/Detect_Loop.java
@@ -0,0 +1,38 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (map.containsKey(temp)) {
+                return true; // cycle found
+            }
+            map.put(temp, 1); // mark node as visited
+            temp = temp.next;
+        }
+
+        return false; // no cycle
+    }
+}
