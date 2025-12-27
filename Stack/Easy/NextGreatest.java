5bb3678a0c7c6ed52ca9cd4c471eb6c51be4ed2a
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-12-27 11:25:46 +0530

    Implement nextLargerElement method in Solution class

--- Stack/Easy/NextGreatest.java
+++ Stack/Easy/NextGreatest.java
@@ -40,3 +40,27 @@ class Solution {
         return arr;
     }
 }
+------------------------------------------------------------------------
+
+
+class Solution {
+    public int[] nextLargerElement(int[] arr) {
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+                while (!s.isEmpty() && s.peek() <= arr[i]) {
+                    s.pop();
+                }
+                if (s.isEmpty()) {
+                    s.push(arr[i]);
+                    arr[i] = -1;
+                } else if (s.peek() > arr[i]) {
+                int temp = s.peek();
+                s.push(arr[i]);
+                arr[i] = temp;
+                }
+
+        }
+        return arr;
+    }
+}
