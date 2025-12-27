bde72a3a97ddc183b734b307b7c417a2c6b4c0ad
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-12-27 13:07:39 +0530

    Add Circular Next Greater Element solution
    Implement a solution to find the next greater elements in a circular array.

---
+++ Stack/Easy/Circular_Next_Greatest.java
@@ -0,0 +1,37 @@
+/*
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+Example 1:
+Input: nums = [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number. 
+The second 1's next greater number needs to search circularly, which is also 2.
+*/
+class Solution {
+    public int[] nextGreaterElements(int[] arr) {
+    Stack<Integer> s = new Stack<>();
+        for (int i = 2*(arr.length) - 1; i >= 0; i--) {
+            int k=i;
+            int indx=i% arr.length;
+                while (!s.isEmpty() && s.peek() <= arr[indx]) {
+                    s.pop();
+                }
+                if (s.isEmpty()) {
+                    s.push(arr[indx]);
+                    if(k<arr.length)
+                    arr[indx] = -1;
+                } 
+                else if (s.peek() > arr[indx]) {
+                int temp = s.peek();
+                s.push(arr[indx]);
+                if(k<arr.length)
+                {
+                     arr[indx] = temp;
+                }
+                }
+        }
+        return arr;    
+    }
+}
