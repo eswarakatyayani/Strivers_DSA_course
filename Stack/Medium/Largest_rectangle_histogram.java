3045676dca74febb0269acb373e305ed8f5f5c55
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-12-29 11:47:46 +0530

    Largest_rectangle_histogram
    Added time and space complexity comments for clarity.

--- Stack/Medium/Largest_rectangle_histogram.java
+++ Stack/Medium/Largest_rectangle_histogram.java
@@ -1,5 +1,8 @@
+/*
+Time Complexity: O(N). Single loop at the end using O(N)
+Space Complexity: O(3N) where 3 is for the stack, left small array and a right small array
+*/
 import java.util.*;
-
 public class Main {
     static class Solution {
         public int largestRectangleArea(int[] heights) {
@@ -46,3 +49,42 @@ public class Main {
         System.out.println("The largest area in the histogram is " + obj.largestRectangleArea(heights));
     }
 }
+
+-------------------------------------------------------------------------------------------------------------
+/*
+Time Complexity: O(N) + O(N). For loop used along with a while loop
+Space Complexity: O(N). Used for stack
+*/
+import java.util.*;
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            // When current bar is less than the bar on top of stack, calculate area
+            while (!stack.isEmpty() && (i == n || heights[stack.peek()] >= (i < n ? heights[i] : 0))) {
+                int height = heights[stack.pop()];
+
+                int width;
+                if (stack.isEmpty()) {
+                    width = i; // All previous were taller
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] histo = {2, 1, 5, 6, 2, 3, 1};
+        System.out.println("The largest area in the histogram is " + sol.largestRectangleArea(histo));
+    }
+}
+
