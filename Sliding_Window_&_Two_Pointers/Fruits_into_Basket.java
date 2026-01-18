c094f98a380d4408944a8f44f93e4115019e0a2d
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-01-18 20:11:17 +0530

    Implement two-pointer approach for fruit basket problem

--- Sliding_Window_&_Two_Pointers/Fruits_into_Basket.java
+++ Sliding_Window_&_Two_Pointers/Fruits_into_Basket.java
@@ -43,3 +43,36 @@ class Solution {
       return max;
     }
 }
+-------------------------------------------------------------------------------------
+//Two pointer with hashmap
+public class FruitInBasket {
+
+  int totalFruit(int[] fruits) {
+
+    Map<Integer, Integer> basket = new HashMap<>();
+    int left = 0;
+    int right = 0;
+    int maxFruits = 0;
+
+    for (right = 0; right < fruits.length; right++) {
+      // Add current to basket
+      int currentCount = basket.getOrDefault(fruits[right], 0);
+      basket.put(fruits[right], currentCount + 1);
+
+      // If basket has more than 2 type of fruits,
+      // start emptying the basket
+      while (basket.size() > 2) {
+        int fruitCount = basket.get(fruits[left]);
+        if (fruitCount == 1)
+          basket.remove(fruits[left]);
+        else
+          basket.put(fruits[left], fruitCount - 1);
+        left++;
+      }
+
+      maxFruits = Math.max(maxFruits, right - left + 1);
+    }
+    return maxFruits;
+  }
+
+}
