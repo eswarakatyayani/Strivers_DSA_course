a4f5084f7a4c9e0b3af868953c64ba517ccdf6ba
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-02-15 19:27:49 +0530

    Refactor KthLargest class to Solution with updated method

--- Heap/KthLargestUsingPriorityQueue.java
+++ Heap/KthLargestUsingPriorityQueue.java
@@ -1,23 +1,20 @@
 import java.util.PriorityQueue;
 
-class KthLargestUsingPriorityQueue {
-
-    static int kthLargest(int[] arr, int k) {
+class Solution {
+    public int kthLargestElement(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int num : arr) {
+        for (int num : nums) {
             pq.offer(num);
             if (pq.size() > k) {
                 pq.poll();
             }
         }
-
         return pq.peek();
     }
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         int[] arr = {3, 2, 1, 5, 6, 4};
         int k = 2;
         System.out.println(kthLargest(arr, k)); // Output: 5
-    }
+     }
 }
+
