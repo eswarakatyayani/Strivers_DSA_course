0bd869510d7747c888829f375d19297989bb52f8
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-03-08 11:49:25 +0530

    Add GroupAnagrams class to group valid anagrams

---
+++ Strings/Easy/Group_Valid_Anagrams.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String word : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(input));
+    }
+}
