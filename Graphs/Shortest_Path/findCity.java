af05fa817fe16cf0479551d63502f900875ffb27
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-04-27 23:17:12 +0530

    Create findCity.java

---
+++ Graphs/Shortest_Path/findCity.java
@@ -0,0 +1,5 @@
+/*
+Find the city with the smallest number of neighbors.
+There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi,weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distance Threshold. Find out a city with the smallest number of cities that are reachable through some path and whose distance is at most Threshold Distance.
+If there are multiple such cities, our answer will be the city with the greatest number.
+*/
