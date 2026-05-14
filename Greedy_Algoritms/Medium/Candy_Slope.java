661d68d5d6fc0fef4a99a65396293c696b452124
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-05-14 14:53:24 +0530

    candy distribution algorithm
    Refactor candy distribution logic to correct peak handling.

--- Greedy_Algoritms/Medium/Candy_Slope.java
+++ Greedy_Algoritms/Medium/Candy_Slope.java
@@ -1,21 +1,27 @@
 class Solution {
+
     public int candy(int[] ratings) {
 
         int n = ratings.length;
+
         if (n == 1)
             return 1;
+
         int sum = 1;
         int i = 1;
 
         while (i < n) {
+
             // Equal ratings
             if (ratings[i] == ratings[i - 1]) {
                 sum += 1;
                 i++;
                 continue;
             }
+
             // Increasing slope
             int peak = 1;
+
             while (i < n && ratings[i] > ratings[i - 1]) {
                 peak++;
                 sum += peak;
@@ -24,21 +30,19 @@ class Solution {
 
             // Decreasing slope
             int down = 1;
+
             while (i < n && ratings[i] < ratings[i - 1]) {
                 sum += down;
-                i++;
-                // Peak correction
-                if (down >= peak)
-                    sum++;
                 down++;
+                i++;
+            }
+
+            // Peak correction
+            if (down > peak) {
+                sum += (down - peak);
             }
         }
 
         return sum;
     }
 }
-
-/*
-Time  : O(N)
-Space : O(1)
-*/
