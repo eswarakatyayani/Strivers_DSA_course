5b6db82fa4e16f004912264b2ec7b32cf60473ae
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-05-30 17:32:50 +0530

    Add problem statement for Gold Bar problem
    Added problem statement and details for the Gold Bar problem.

--- Dynamic_Programming/Medium/Gold_Bar.java
+++ Dynamic_Programming/Medium/Gold_Bar.java
@@ -2,3 +2,26 @@
 // Longest Common sequence
 // Knapsack Problem
 // Largest Subarray Sum
+
+/*
+Problem Statement
+
+A thief enters a gold mine carrying a bag with capacity W kg.
+There are N gold bars, where:
+
+weight[i] = weight of the ith gold bar
+value[i] = value of the ith gold bar
+
+The thief can either: Take the entire gold bar or Leave it. But he cannot break a gold bar.
+
+Find the maximum value that can be carried in the bag.
+Capacity W = 10
+
+Gold Bars:
+Weight  Value
+1       60
+2       100
+3       120
+
+Output: 280
+*/
