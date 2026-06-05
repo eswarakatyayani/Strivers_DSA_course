8d4ed2d842e24bc72f4c8b095291d3c8d02e16c9
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-06-05 16:08:18 +0530

    Implement Target Sum solution with memoization

--- Dynamic_Programming/Subsequences/Target_Sum/Target_Sum.java
+++ Dynamic_Programming/Subsequences/Target_Sum/Target_Sum.java
@@ -1,1 +1,28 @@
-//
+import java.util.*;
+
+class Solution {
+
+    static final int MOD = 1000000007;
+    Map<String, Integer> memo = new HashMap<>();
+    public int findTargetSumWays(int[] nums, int target) {
+        return solve(0, 0, nums, target);
+    }
+    private int solve(int index, int sum, int[] nums, int target) {
+
+        if (index == nums.length) {
+            return sum == target ? 1 : 0;
+        }
+        String key = index + "," + sum;
+
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        long positive = solve( index + 1, sum + nums[index], nums, target);
+        long negative = solve(index + 1, sum - nums[index], nums, target);
+        int ans = (int)((positive + negative) % MOD);
+        memo.put(key, ans);
+
+        return ans;
+    }
+}
