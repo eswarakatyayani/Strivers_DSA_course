4b5ed4c0978bae46779bad6a0c635a3d61244a6c
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-06-11 19:24:47 +0530

    Implement longest common substring algorithm

--- Dynamic_Programming/String/Longest_Common_Substring.java
+++ Dynamic_Programming/String/Longest_Common_Substring.java
@@ -1,1 +1,44 @@
 // Longest common substring
+import java.util.*;
+
+// Class containing the LCS logic
+class Solution {
+    /* Function to find the length of 
+       the Longest Common Substring */
+    public int longestCommonSubstr(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+    
+        // DP table: dp[i][j] stores length of common substring ending at str1[i-1] and str2[j-1]
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Variable to store the maximum substring length found so far
+        int ans = 0;
+
+        // Fill the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // If characters match, extend the substring
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = Math.max(ans, dp[i][j]); // Update maximum length
+                } else {
+                    dp[i][j] = 0; // Reset length when characters don't match
+                }
+            }
+        }
+        return ans; // Return the longest length found
+    }
+}
+
+// Separate main class for testing
+class Main {
+    public static void main(String[] args) {
+        String s1 = "abcjklp";
+        String s2 = "acjkp";
+
+        Solution sol = new Solution();
+        System.out.println("The Length of Longest Common Substring is " + sol.longestCommonSubstr(s1, s2));
+    }
+}
+
